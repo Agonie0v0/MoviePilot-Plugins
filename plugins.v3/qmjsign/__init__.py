2d4a54005a82f827de07eadffbf70b89d9abe49b
Agonie <107966975+Agonie0v0@users.noreply.github.com>
2026-09-08 10:25:56 +0800

    chore(release): bump version to 1.2.1

--- plugins.v3/qmjsign/__init__.py
+++ plugins.v3/qmjsign/__init__.py
@@ -106,7 +106,7 @@ class qmjsign(_PluginBase):
     # 插件图标
     plugin_icon = "qmj.ico"
     # 插件版本
-    plugin_version = "1.2.0"
+    plugin_version = "1.2.1"
     # 插件作者
     plugin_author = "Agonie"
     # 作者主页
